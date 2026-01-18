47af91554ad4ea9f04d3d1f98990ea62bdd67a46
Phan Lam Dung <phanlamdung2006@gmail.com>
2026-01-18 12:42:56 +0700

    update

--- sysmetric/diffusion.py
+++ sysmetric/diffusion.py
@@ -9,12 +9,10 @@ def inv_shift_rows(s):
     s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
     s[0][3], s[1][3], s[2][3], s[3][3] = s[1][3], s[2][3], s[3][3], s[0][3]
     
-# learned from http://cs.ucsb.edu/~koc/cs178/projects/JT/aes.c
 xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
 
 
 def mix_single_column(a):
-    # see Sec 4.1.2 in The Design of Rijndael
     t = a[0] ^ a[1] ^ a[2] ^ a[3]
     u = a[0]
     a[0] ^= t ^ xtime(a[0] ^ a[1])
@@ -55,4 +53,5 @@ print(-1%4)
 inv_mix_columns(state)
 inv_shift_rows(state)
 print(matrix2bytes(state))
+
 
